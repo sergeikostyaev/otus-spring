0bf2dcf7cd1f54fba349070edcf65568585b2e72
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-09-23 15:20:32 +0300

    hw

--- hw05-librarySpringJdbc/src/main/java/ru/otus/spring/service/impl/LibraryServiceImpl.java
+++ hw05-librarySpringJdbc/src/main/java/ru/otus/spring/service/impl/LibraryServiceImpl.java
@@ -1,8 +1,33 @@
 package ru.otus.spring.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.spring.dao.LibraryRepository;
+import ru.otus.spring.domain.Book;
 import ru.otus.spring.service.LibraryService;
 
 @Service
+@RequiredArgsConstructor
 public class LibraryServiceImpl implements LibraryService {
+    private final LibraryRepository libraryRepository;
+
+    @Override
+    public void insertBook(String bookName, Long authorId, Long genreId) {
+        libraryRepository.insertBook(bookName, authorId, genreId);
+    }
+
+    @Override
+    public Book findBookById(Long id) {
+        return libraryRepository.findBookById(id);
+    }
+
+    @Override
+    public void updateBookById(Long id, String name, Long authorId, Long genreId) {
+        libraryRepository.updateBookById(id, name, authorId, genreId);
+    }
+
+    @Override
+    public void removeBookById(Long id) {
+        libraryRepository.removeBookById(id);
+    }
 }
--- hw05-librarySpringJdbc/src/main/java/ru/otus/spring/shell/ShellController.java
+++ hw05-librarySpringJdbc/src/main/java/ru/otus/spring/shell/ShellController.java
@@ -6,34 +6,35 @@ import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 import ru.otus.spring.dao.LibraryRepository;
 import ru.otus.spring.service.IoService;
+import ru.otus.spring.service.LibraryService;
 
 @ShellComponent
 @RequiredArgsConstructor
 public class ShellController {
 
-    private final LibraryRepository libraryRepository;
+    private final LibraryService libraryService;
     private final IoService ioService;
 
     @ShellMethod(key = {"fb", "find book"})
     public void findBookById(long id){
-        ioService.print(String.valueOf(libraryRepository.findBookById(id)));
+        ioService.print(String.valueOf(libraryService.findBookById(id)));
     }
 
     @ShellMethod(key = {"ab", "add book"})
     public void addBook(String name, long authorId, long genreId){
-        libraryRepository.insertBook(name, authorId, genreId);
+        libraryService.insertBook(name, authorId, genreId);
         ioService.print("Book " + name + " added");
     }
 
     @ShellMethod(key = {"db", "delete book"})
     public void deleteBook(long id){
-        libraryRepository.removeBookById(id);
+        libraryService.removeBookById(id);
         ioService.print("Book with id " + id + " deleted");
     }
 
     @ShellMethod(key = {"ub", "update book"})
     public void updateBook(long id, String bookName, Long authorId, Long genreId){
-        libraryRepository.updateBookById(id, bookName, authorId, genreId);
+        libraryService.updateBookById(id, bookName, authorId, genreId);
         ioService.print("Book with id " + id + " updated");
     }
 
