d372bb13a0c403e2d95ec82d2760a63c70c49892
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-10-06 11:12:35 +0300

    hw

--- hw06-librarySpringJpql/src/main/java/ru/otus/spring/domain/Book.java
+++ hw06-librarySpringJpql/src/main/java/ru/otus/spring/domain/Book.java
@@ -23,11 +23,11 @@ public class Book {
     private String name;
 
     @JoinColumn(name = "author_id")
-    @OneToOne(targetEntity = Author.class, cascade = CascadeType.MERGE)
+    @OneToOne(targetEntity = Author.class, cascade = CascadeType.DETACH)
     private Author author;
 
     @JoinColumn(name = "genre_id")
-    @OneToOne(targetEntity = Genre.class, cascade = CascadeType.MERGE)
+    @OneToOne(targetEntity = Genre.class, cascade = CascadeType.DETACH)
     private Genre genre;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "book")
