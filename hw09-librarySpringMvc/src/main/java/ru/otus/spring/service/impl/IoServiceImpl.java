fd2d919c715f3a2438b75d7158f57a2d88d9a353
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-11-12 14:42:43 +0300

    hw

--- hw09-librarySpringMvc/src/main/java/ru/otus/spring/service/impl/IoServiceImpl.java
+++ hw09-librarySpringMvc/src/main/java/ru/otus/spring/service/impl/IoServiceImpl.java
@@ -3,8 +3,6 @@ package ru.otus.spring.service.impl;
 import org.springframework.stereotype.Service;
 import ru.otus.spring.service.IoService;
 
-import java.util.Scanner;
-
 @Service
 public class IoServiceImpl implements IoService {
 
