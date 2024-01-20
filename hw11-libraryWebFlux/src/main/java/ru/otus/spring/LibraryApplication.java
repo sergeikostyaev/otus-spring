1373632f9447ef427b15722e3cb6c3db3129df83
sergeikostyaev <sergei.kostyaev88@gmail.com>
2024-01-20 13:34:27 +0300

    hw11

---
+++ hw11-libraryWebFlux/src/main/java/ru/otus/spring/LibraryApplication.java
@@ -0,0 +1,17 @@
+package ru.otus.spring;
+
+import org.h2.tools.Console;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) throws SQLException {
+        //Console.main(args);
+        System.out.println("http://localhost:8080");
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
