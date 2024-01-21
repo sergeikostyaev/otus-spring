280e77f2598e6b8b972dc791756a5b587d03d5f9
sergeikostyaev <sergei.kostyaev88@gmail.com>
2024-01-21 10:26:31 +0300

    hw11

--- hw11-libraryWebFlux/src/main/java/ru/otus/spring/LibraryApplication.java
+++ hw11-libraryWebFlux/src/main/java/ru/otus/spring/LibraryApplication.java
@@ -1,8 +1,14 @@
 package ru.otus.spring;
 
-import org.h2.tools.Console;
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 import java.sql.SQLException;
 
@@ -10,8 +16,22 @@ import java.sql.SQLException;
 public class LibraryApplication {
 
     public static void main(String[] args) throws SQLException {
-        //Console.main(args);
         System.out.println("http://localhost:8080");
         SpringApplication.run(LibraryApplication.class, args);
     }
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        Resource schemaScript = new ClassPathResource("schema.sql");
+        Resource dataScript = new ClassPathResource("data.sql");
+        ResourceDatabasePopulator schemaPopulator = new ResourceDatabasePopulator(schemaScript);
+        ResourceDatabasePopulator dataPopulator = new ResourceDatabasePopulator(dataScript);
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator(schemaPopulator, dataPopulator);
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
+    }
+
 }
--- hw11-libraryWebFlux/src/main/java/ru/otus/spring/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.spring.domain.Book;
-
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
