e6b67f179f9e87cdfa7d2e75006c811e38fffbfe
sergeikostyaev <116896578+sergeikostyaev@users.noreply.github.com>
2024-07-10 19:25:12 +0300

    hw14

--- hw14-springBatch/src/main/java/ru/otus/spring/domain/mongo/BookMongo.java
+++ hw14-springBatch/src/main/java/ru/otus/spring/domain/mongo/BookMongo.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class BookMongo {
 
     @Id
-    private String id = UUID.randomUUID().toString();
+    private final String id = UUID.randomUUID().toString();
 
     private String name;
 
--- hw14-springBatch/src/main/java/ru/otus/spring/mapper/impl/BookMapper.java
+++ hw14-springBatch/src/main/java/ru/otus/spring/mapper/impl/BookMapper.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Component;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.domain.mongo.Author;
 import ru.otus.spring.domain.mongo.BookMongo;
+import ru.otus.spring.domain.mongo.Comment;
 import ru.otus.spring.domain.mongo.Genre;
 import ru.otus.spring.mapper.EntityMapper;
+
+import java.util.stream.Collectors;
 
 @Component
 public class BookMapper implements EntityMapper<Book, BookMongo> {
     @Override
     public BookMongo map(Book book) {
-        return BookMongo.builder().name(book.getName()).author(Author.builder().name(book.getAuthor().getName()).build())
-                .genre(Genre.builder().name(book.getGenre().getName()).build()).build();
+        return new BookMongo(book.getName(), new Author(book.getAuthor().getName()),
+                new Genre(book.getGenre().getName()), book.getComments().stream().map(c -> new Comment(c.getComment())).collect(Collectors.toList()));
     }
 }
