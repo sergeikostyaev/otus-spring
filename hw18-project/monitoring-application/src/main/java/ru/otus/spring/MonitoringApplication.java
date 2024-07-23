ba9a4aff085302ecdd09b1ec1192e0d939805c37
sergeikostyaev <116896578+sergeikostyaev@users.noreply.github.com>
2024-07-23 17:29:55 +0300

    hw18

--- hw18-project/monitoring-application/src/main/java/ru/otus/spring/MonitoringApplication.java
+++ hw18-project/monitoring-application/src/main/java/ru/otus/spring/MonitoringApplication.java
@@ -1,19 +1,15 @@
 package ru.otus.spring;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
-
 @EnableAdminServer
 @SpringBootApplication
-public class BotApplication {
+public class MonitoringApplication {
 
-    public static void main(String[] args) throws SQLException {
-        Console.main(args);
-        SpringApplication.run(BotApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
     }
 
 }
